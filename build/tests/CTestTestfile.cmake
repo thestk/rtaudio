3b9f006a842689bd168e5e0766587585a3228e51
ColinGilbert <colingilbert86@gmail.com>
2015-04-14 14:12:20 -0600

    Fix compilation on Linux (CMake)

--- build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/noob/rtaudio/tests
-# Build directory: /home/noob/rtaudio/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
