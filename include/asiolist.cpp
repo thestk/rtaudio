820b138b8231fede8b8fc04a336baa0875381a23
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2021-05-02 04:30:00 +0200

    asiolist.cpp: remove unused variable

--- include/asiolist.cpp
+++ include/asiolist.cpp
@@ -166,7 +166,6 @@ AsioDriverList::AsioDriverList ()
 	LPASIODRVSTRUCT	pdl;
 	LONG 			cr;
 	DWORD			index = 0;
-	BOOL			fin = FALSE;
 
 	numdrv		= 0;
 	lpdrvlist	= 0;
@@ -184,7 +183,6 @@ AsioDriverList::AsioDriverList ()
 #endif
 			lpdrvlist = newDrvStruct (hkEnum,keyname,0,lpdrvlist);
 		}
-		else fin = TRUE;
 	}
 	if (hkEnum) RegCloseKey(hkEnum);
 
